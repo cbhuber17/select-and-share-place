cf3899bfeacbd5b109f3fe8ddf03118793c9eb4c
Colin Huber <cbhuber@gmail.com>
2023-08-19 16:46:25 -0600

    feat: get lat/lon from address.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,35 @@
-// Code goes here!
+import axios from "axios";
+import "dotenv/config";
+
+require("dotenv").config({ path: __dirname + "/./../.env" });
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${process.env.GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
